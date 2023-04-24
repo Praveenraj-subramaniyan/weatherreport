5e8b29b3dfef6b0e49cfce6c880f04c3ca4370e9
spr887011 <spr887011@gmail.com>
2023-04-24 20:39:34 +0530

    Arranged in alphabetical order

--- src/App.js
+++ src/App.js
@@ -13,24 +13,28 @@ function App() {
     fetch(`https://restcountries.com/v3.1/all`)
       .then((response) => response.json())
       .then((data) => {
+        data.sort((a, b) => {
+          if (a.name.common < b.name.common) {
+            return -1;
+          }
+          if (a.name.common > b.name.common) {
+            return 1;
+          }
+          return 0;
+        });
         setcountriesData(data);
-        // countriesData.forEach((country)=>{
-        //   console.log(` ${country.latlng}`);
-        // });
       })
       .catch((error) => console.log(error));
   }
 
     function fetchweatherData(latlng) {
-      // let value=latlng.split(",")
       fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latlng[0]}&lon=${latlng[1]}&appid=12f4cfff5cf2766bc4f975fdc6c3b200`)
         .then((response) => response.json())
         .then((data) => {
           setWeatherData(data);
         })
         .catch((error) => console.log(error));
-        console.log(latlng[0]);
-        console.log(latlng[1]);
+        console.log(weatherData);
     }
  
 
